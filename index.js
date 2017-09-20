0de5b4c213ab403a229bbb589c677af344aacc3c
Pauli Payne <pauli.a.payne@gmail.com>
2017-09-20 20:07:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-console.error("HALP!")
-console.log("I would be a logger.")
-console.warn("I'm warning you!")
+console.error("HALP!");
+console.log("I would be a logger.");
+console.warn("I'm warning you!");
